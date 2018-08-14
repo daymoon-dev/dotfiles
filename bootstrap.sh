e40eeeaaab6ccab982f5887baad73b0e8a343508
daymoon89 <33713828+daymoon89@users.noreply.github.com>
2018-08-14 12:42:49 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
-# 콘솔에서 라이선스 동의하기
-sudo xcodebuild -license
-
 # Command Line Tool 설치
 xcode-select --install
+
+# 콘솔에서 라이선스 동의하기
+sudo xcodebuild -license
 
 # Set temporary variable
 DOTFILES=$HOME/dotfiles
